f4881809b592f26c613377f7ad4bbdd1168d601b
john <johnhowardm@hotmail.com>
2024-01-23 15:47:23 +0000

    Removing a console.log

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -16,8 +16,6 @@ const Page = async ({
 
   const movies = await getMovies(skip, limit);
 
-  console.log('movies: ', movies);
-
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-slate-300'>
       <div>
