e2646904e2e93fc3486888a5f0015c960e3a5194
john <johnhowardm@hotmail.com>
2024-01-23 15:46:10 +0000

    Changing tailwind coding so that loading page displays as a clean single page

--- app/movies/loading.tsx
+++ app/movies/loading.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 export const Loading = () => {
   return (
-    <section className='py-24'>
+    // <section className='py-24'>
+    <section className='flex min-h-screen flex-col items-center justify-between p-24 bg-slate-300'>
       <div className='container'>
         <h2>...loading</h2>
       </div>
