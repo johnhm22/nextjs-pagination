88670c066ac16b0992ef3c1c0ed212b1ab736585
john <johnhowardm@hotmail.com>
2024-01-23 15:06:07 +0000

    Deleting commented out code

--- lib/mongodb/movies.ts
+++ lib/mongodb/movies.ts
@@ -33,8 +33,6 @@ export const getMovies = async (skip: number, limit: number) => {
     let moviesArray = [];
     if (!movies) await init();
     const result = await movies.find().limit(limit).skip(skip);
-    //   .map((user) => ({ ...user, _id: user._id.toString() }))
-    //   .toArray();
 
     for await (const mov of result) {
       moviesArray.push(mov);
@@ -43,37 +41,8 @@ export const getMovies = async (skip: number, limit: number) => {
     const countOfMovies = await movies.count();
     console.log('countOfMovies: ', countOfMovies);
 
-    // console.log(moviesArray);
     return moviesArray;
   } catch (error) {
     return { error: 'failed to fetch movies' };
   }
 };
-
-//database name
-// const dbName = 'sample_mflix';
-
-// export const run = async () => {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db('admin').command({ ping: 1 });
-//     console.log(
-//       'Pinged your deployment. You successfully connected to MongoDB!'
-//     );
-//     let moviesArray = [];
-//     const db = client.db(dbName);
-//     const collection = db.collection('movies').find().limit(10);
-
-//     for await (const mov of collection) {
-//       moviesArray.push(mov);
-//     }
-//     console.log(moviesArray);
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// };
-
-// run().catch(console.dir);
